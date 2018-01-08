0fba05e59062af9aa76aaec541d5ed152356dbd5
Nathan Fox <fuchsnj@gmail.com>
2018-01-07 20:33:56 -0500

    fix formatting

--- src/game/team.rs
+++ src/game/team.rs
@@ -2,29 +2,29 @@ use game::data::GameData;
 
 #[derive(Clone)]
 pub struct Team {
-    id: String,
-    name: String,
-    password: String,
+	id: String,
+	name: String,
+	password: String,
 }
 
 impl Team {
-    pub fn new(name: &str, password: &str) -> Team {
-        Team {
-            id: GameData::generate_id(),
-            name: name.to_owned(),
-            password: password.to_owned(),
-        }
-    }
+	pub fn new(name: &str, password: &str) -> Team {
+		Team {
+			id: GameData::generate_id(),
+			name: name.to_owned(),
+			password: password.to_owned(),
+		}
+	}
 
-    pub fn get_id(&self) -> String {
-        self.id.clone()
-    }
+	pub fn get_id(&self) -> String {
+		self.id.clone()
+	}
 
-    pub fn get_name(&self) -> String {
-        self.name.clone()
-    }
+	pub fn get_name(&self) -> String {
+		self.name.clone()
+	}
 
-    pub fn check_password(&self, password: &str) -> bool {
-        self.password == password //TODO: fix timing attack
-    }
+	pub fn check_password(&self, password: &str) -> bool {
+		self.password == password //TODO: fix timing attack
+	}
 }
